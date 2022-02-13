d036e64ad33042cc3e55e7eb8063085097f19dbf
Ketlinl <ketlinlarissa37@gmail.com>
2022-02-13 18:12:35 -0300

    Fix admin permissions

--- project/users/admin.py
+++ project/users/admin.py
@@ -1,9 +1,30 @@
-from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.admin import GroupAdmin as BaseGroupAdmin, UserAdmin as BaseUserAdmin
 from django.contrib.auth.models import Group
 from django.contrib import admin
 from django.db.models import Q
 from .forms import UserCreationForm, UserForm
 from .models import User
+
+
+class GroupAdmin(BaseGroupAdmin):
+    """
+    Sobrescrever as permissões removendo as não usadas.
+    """
+
+    def formfield_for_manytomany(self, db_field, request=None, **kwargs):
+        """
+        Formata os campos de many to many das permissões.
+        """
+
+        if db_field.name == 'permissions':
+            queryset = kwargs.get('queryset', db_field.remote_field.model.objects)
+            kwargs['queryset'] = queryset.filter(
+                ~Q(content_type__app_label__in=[
+                    "admin", "auth", "contenttypes", "sessions"
+                ])
+            )
+
+        return super().formfield_for_manytomany(db_field, request=request, **kwargs)
 
 
 class UserAdmin(BaseUserAdmin):
@@ -19,15 +40,37 @@ class UserAdmin(BaseUserAdmin):
 
     add_fieldsets = ((None, {'fields': ('email', 'name', 'password1', 'password2')}),)
 
-    list_display = ('name', 'email', 'is_active', 'is_staff', 'created_at')
+    list_display = ('name', 'email', 'is_active', 'is_staff', 'is_superuser', 'created_at')
 
     search_fields = ('name', 'email')
 
-    list_filter = ('created_at', 'is_active', 'is_staff')
+    list_filter = ('created_at', 'is_active', 'is_staff', 'is_superuser')
 
     ordering = ('email', )
 
     actions = ('active_user', 'desactive_user')
+
+    def get_fieldsets(self, request, obj=None):
+        """
+        Super usuários podem modificar permissões.
+        """
+
+        if not obj:
+            return self.add_fieldsets
+
+        fieldsets = super().get_fieldsets(request, obj)
+
+        if request.user.is_superuser:
+            fieldsets = (
+                (None, {'fields': ('email', 'name')}),
+                ('PERMISSÕES', {
+                    'fields': ('is_active', 'is_staff', 'groups'),
+                    'description': 'Sistema de permissões do usuário.',
+                    'classes': ('extrapretty', 'collapse', 'in')
+                })
+            )
+
+        return fieldsets
 
     def get_ordering(self, request):
         """
@@ -56,8 +99,21 @@ class UserAdmin(BaseUserAdmin):
 
     desactive_user.short_description = 'Desativa usuários'
 
+    def get_queryset(self, request):
+        """
+        Modifica o comportamento da listagem
+        """
+
+        queryset = super().get_queryset(request)
+        if request.user.is_superuser:
+            return queryset
+
+        queryset = queryset.filter(email=request.user.email)
+        return queryset
+
 
 admin.site.unregister(Group)
+admin.site.register(Group, GroupAdmin)
 admin.site.register(User, UserAdmin)
 admin.site.site_header = "Chatbot Theo"
 admin.site.site_title = "Parte Administrativa"
