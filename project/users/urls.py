917cd07ac00e82806b957cea8421a7f5f3b14fef
Ketlinl <ketlinlarissa37@gmail.com>
2022-02-12 23:27:57 -0300

    Remove unused code

--- project/users/urls.py
+++ project/users/urls.py
@@ -3,13 +3,5 @@ from project.users import views
 
 urlpatterns = [
     # path('login/', views.login),
-    # path('entrar/', views.auth),
-    # path('', views.users),
-    # path('<int:id>/', views.user),
-    # path('criar/', views.create_user),
-    # path('salvar-criacao/', views.save_creation),
-    # path('<int:id>/editar/', views.update_user),
-    # path('salvar-edicao/', views.save_edition),
-    # path('<int:id>/deletar/', views.delete_user),
-    # path('salvar-delecao/', views.save_deletion)
+    # path('entrar/', views.auth)
 ]
